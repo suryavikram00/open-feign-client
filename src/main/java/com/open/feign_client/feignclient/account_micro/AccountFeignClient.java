9166b6a7be7239323faae54bd460824fca36eb47
Surya Vikram <surya.vikram@freshworks.com>
2024-10-27 13:35:03 +0530

    added feign client

--- src/main/java/com/open/feign_client/feignclient/account_micro/AccountFeignClient.java
+++ src/main/java/com/open/feign_client/feignclient/account_micro/AccountFeignClient.java
@@ -3,8 +3,8 @@ package com.open.feign_client.feignclient.account_micro;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.api.open.crud.api.entity.account_micro.AccountEntityModel;
-import com.api.open.crud.api.feignclient.OpenFeignClient;
+import com.open.feign_client.entity.account_micro.AccountEntityModel;
+import com.open.feign_client.feignclient.OpenFeignClient;
 
 @FeignClient(name = "accountClient", url = "${account.client.url}/account")
 public interface AccountFeignClient extends OpenFeignClient<AccountEntityModel> {  
--- src/main/java/com/open/feign_client/feignclient/user_micro/UserFeignClient.java
+++ src/main/java/com/open/feign_client/feignclient/user_micro/UserFeignClient.java
@@ -3,8 +3,9 @@ package com.open.feign_client.feignclient.user_micro;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
-import com.api.open.crud.api.entity.user_micro.UserEntityModel;
-import com.api.open.crud.api.feignclient.OpenFeignClient;
+import com.open.feign_client.entity.user_micro.UserEntityModel;
+import com.open.feign_client.feignclient.OpenFeignClient;
+
 
 @FeignClient(name = "userClient", url = "${user.client.url}/user")
 public interface UserFeignClient extends OpenFeignClient<UserEntityModel> {  
